f54b1f2fb0eefedf67b22488b330380beb850f9b
Maryem GHRIBI <mariemghribi0289@gmail.com>
2024-03-23 15:52:14 +0100

    ✨feature: add mock data

---
+++ src/models/mock-nakama.ts
@@ -0,0 +1,96 @@
+import Nakama from './nakama';
+   
+export const NAKAMAS: Nakama[] = [
+ {
+  id: 1,
+  name: "Luffy",
+  hp: 25,
+  cp: 5,
+  picture: "https://picfiles.alphacoders.com/621/621078.jpeg",
+  types: ["Captin pirate"],
+  created: new Date()
+ },
+ {
+  id: 2,
+  name: "Zoro",
+  hp: 28,
+  cp: 6,
+  picture: "https://images3.alphacoders.com/134/1345517.jpeg",
+  types: ["Epéiste"],
+  created: new Date()
+ },
+ {
+  id: 3,
+  name: "Nami",
+  hp: 21,
+  cp: 4,
+  picture: "https://avatarfiles.alphacoders.com/104/104441.jpg",
+  types: ["Navigatrice"],
+  created: new Date()
+ },
+ {
+  id: 4,
+  name: "Ussop",
+  hp: 16,
+  cp: 2,
+  picture: "https://picfiles.alphacoders.com/555/555415.jpg",
+  types: ["Mécanicien"],
+  created: new Date()
+ },
+ {
+  id: 5,
+  name: "Sanji",
+  hp: 30,
+  cp: 7,
+  picture: "https://avatarfiles.alphacoders.com/874/87484.jpg",
+  types: ["Cuistot"],
+  created: new Date()
+ },
+ {
+  id: 6,
+  name: "Tony-Tony Chopper",
+  hp: 18,
+  cp: 6,
+  picture: "https://avatarfiles.alphacoders.com/321/321054.png",
+  types: ["Médecin"],
+  created: new Date()
+ },
+ {
+  id: 7,
+  name: "Robin",
+  hp: 14,
+  cp: 5,
+  picture: "https://images3.alphacoders.com/164/thumb-440-164945.webp",
+  types: ["Archéologue"],
+  created: new Date()
+ },
+ {
+  id: 8,
+  name: "Franky",
+  hp: 16,
+  cp: 4,
+  picture: "https://images8.alphacoders.com/132/thumb-440-1326576.webp",
+  types: ["Charpentier"],
+  created: new Date()
+ },
+ {
+  id: 9,
+  name: "Brook",
+  hp: 21,
+  cp: 7,
+  picture: "https://images7.alphacoders.com/132/1327087.png",
+  types: ["Sool-King"],
+  created: new Date()
+ },
+ {
+  id: 10,
+  name: "Jinbe",
+  hp: 19,
+  cp: 3,
+  picture: "https://images6.alphacoders.com/122/thumb-440-1225550.webp",
+  types: ["Le paladin des mers"],
+  created: new Date()
+ }
+];
+  
+export default NAKAMAS;
