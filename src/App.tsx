ac090f76c614c6b433be05aebc213fd3eb134633
Maryem GHRIBI <mariemghribi0289@gmail.com>
2024-03-23 21:39:33 +0100

    ✨feature: afficher la liste des nakamas avec materializecss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Nakama from './models/nakama';
+import NAKAMAS from './models/mock-nakama';
 
 function App() {
+  const [nakamas, setNakamas] = useState<Nakama[]>([]);
+  
+  useEffect(()=>{
+    setNakamas(NAKAMAS)
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='center'>One Piece</h1>
+      <div className='container'>
+        <div className='row'>
+          {nakamas.map(({id, name, picture, created}) => (
+            <><div className='card horizontal'>
+              <img src={picture} alt={name} />
+            </div><div className='card-stacked'>
+                <div className='card-content'>
+                  <p>{name}</p>
+                  <p><small>{created.toString()}</small></p>
+                </div>
+              </div></>
+          ))}
+
+        </div>
+
+      </div>
     </div>
   );
 }
